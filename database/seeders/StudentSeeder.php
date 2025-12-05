107778db61534f6dd92b0c14e0100be2acdfd1df
maze272003 <jg.jonatas.au@phinmaed.com>
2025-12-05 10:26:59 +0800

    add seeder

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -13,15 +13,25 @@ class StudentSeeder extends Seeder
         // 1. CREATE SPECIFIC STUDENT (JM)
         Student::create([
             'lrn' => '100000000001', // Custom LRN
-            'first_name' => 'JM',
-            'last_name' => 'Jonatas',
-            'email' => 'jmjonatas4@gmail.com',
+            'first_name' => 'Developer',
+            'last_name' => 'Developers',
+            'email' => 'dev@gmail.com',
             'password' => Hash::make('password'), // Encrypt ang password
             'grade_level' => 12,    // Example lang
             'strand' => 'STEM',     // Example lang
             'section' => 'Rizal',   // Example lang
         ]);
 
+        Student::create([
+            'lrn' => '100000000002', // Custom LRN
+            'first_name' => 'Elice',
+            'last_name' => 'Erman',
+            'email' => 'elicegerman@gmail.com',
+            'password' => Hash::make('password'), // Encrypt ang password
+            'grade_level' => 12,    // Example lang
+            'strand' => 'STEM',     // Example lang
+            'section' => 'Rizal',   // Example lang
+        ]);
         // 2. CREATE RANDOM STUDENTS (49 others)
         // Siguraduhin na ang Factory mo ay naglalagay din ng default password
         Student::factory(1500)->create([
